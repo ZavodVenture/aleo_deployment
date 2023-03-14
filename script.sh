ba01283d5d0acd2c94028ce4e79f9f951ee12f75
ZavodVenture <127696238+ZavodVenture@users.noreply.github.com>
2023-03-14 17:45:29 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,10 @@
-WALLETADDRESS=""
+WALLETADDRESS="aleo1gmg5x7jyv330kq78j2jll69scxq84jnxclaq5wrqeyme88whvqxssd0c9h"
 APPNAME="helloworld_${WALLETADDRESS:4:6}"
 PRIVATEKEY=""
 RECORD=""
 
-sudo apt update -y
+clear
+echo -e "\033[0;33mStarting...\033[0m\n"
 sudo apt install -y build-essential pkg-config libssl-dev curl clang gcc llvm make tmux xz-utils ufw
 echo -ne "\n" | curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env"
@@ -17,8 +18,7 @@ cargo install --path .
 sudo ufw allow 4133/tcp
 sudo ufw allow 3033/tcp
 cd ~
-sudo rm -r leo
-sudo rm -r snarkOS
+sudo rm -r leo snarkOS
 leo new "$APPNAME"
 cd "$APPNAME" && leo run && cd -
 PATHTOAPP=$(realpath -q $APPNAME)
@@ -26,7 +26,7 @@ cd $PATHTOAPP && cd ..
 snarkos developer deploy "$APPNAME.aleo" --private-key "$PRIVATEKEY" --query "https://vm.aleo.org/api" --path "./$APPNAME/build/" --broadcast "https://vm.aleo.org/api/testnet3/transaction/broadcast" --fee 600000 --record "$RECORD"
 clear
 echo -e "\033[32mDeployment finished!\033[0m\n"
-echo -e "Aleo app name: \033[33m$APPNAME.aleo\033[0m\n"
+echo -e "Aleo app name: \033[33m$APPNAME\033[0m\n"
 echo -e "\033[0;33mCREATED BY ZAVOD VENTURE\033[0m"
 echo -e "\033[33mTelegram: \033[36mhttps://t.me/Zavod_Venture\033[0m\n"
 echo -e "\033[33mDonation:\033[0m"
